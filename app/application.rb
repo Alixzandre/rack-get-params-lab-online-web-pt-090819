9afe01c986f3e9fdbb0e8bc5d8fdcb336292f2e9
Alexander Bispo <alexbispo173@gmail.com>
2019-12-09 15:31:21 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -27,7 +27,7 @@ class Application
       if @@items.include?(item)
         @@cart << item
       else
-        resp.write ""
+        resp.write "We don't have that item"
       end
     else
       resp.write "Path Not Found"
