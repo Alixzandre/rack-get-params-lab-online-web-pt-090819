9a4cddd7e51fb967874e1f6ea8cb89fe525f68a9
Alexander Bispo <alexbispo173@gmail.com>
2019-12-09 15:31:51 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -26,6 +26,7 @@ class Application
       item = req.params["item"]
       if @@items.include?(item)
         @@cart << item
+        
       else
         resp.write "We don't have that item"
       end
